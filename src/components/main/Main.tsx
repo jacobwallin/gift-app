126a8d7019c557bb418e667023204e2f677c96da
Jacob Wallin <jacobwallin@gmail.com>
2022-12-01 01:04:39 -0600

    build out initial ui

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
 import { useSession } from "next-auth/react";
+import { trpc } from "../../utils/trpc";
+import Gift from "./Gift";
 
 export default function Main() {
+  const { data } = trpc.gifts.getAll.useQuery();
   const { data: sessionData } = useSession();
-  return <div>{`Welcome ${sessionData?.user?.name}`}</div>;
+
+  const [selectedUserId, setSelectedUserId] = useState(
+    sessionData?.user?.id ?? ""
+  );
+
+  return (
+    <div className="mt-20 flex justify-center gap-5">
+      <div className=" flex min-h-[400px] w-[175px] flex-col rounded-lg bg-white p-4 shadow-md">
+        <div className="text-xl">My List</div>
+        <div>Family Lists</div>
+      </div>
+      <div className="flex flex-col gap-4 ">
+        <div className="border-1 border-gray flex h-[40px] w-[600px] flex-col rounded-lg bg-sky-700 shadow-md">
+          Filter / Action Bar
+        </div>
+        <div className="border-1 border-gray flex w-[600px] flex-col rounded-lg bg-white shadow-md">
+          <Gift />
+          <Gift />
+          <Gift />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,17 +29,8 @@ export default Home;
 const Auth: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
       <button
         className="rounded-full bg-black px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={() => signIn("google")}
