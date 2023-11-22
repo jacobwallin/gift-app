8eb578da09f41997c4a1b0f56235a97085638606
Jacob Wallin <jacobwallin@gmail.com>
2023-11-22 00:33:58 -0600

    fix import filename mismatch

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { emailWhitelist, devEmailWhitelist } from "./emailWhiteList";
+import { emailWhitelist, devEmailWhitelist } from "./emailWhitelists";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
