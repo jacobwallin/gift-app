94b37f21670a8b057f7f72f36c984f4dbc1af428
Jacob Wallin <jacobwallin@gmail.com>
2023-11-22 00:36:59 -0600

    fix typo

--- src/server/trpc/router/users.ts
+++ src/server/trpc/router/users.ts
@@ -7,14 +7,12 @@ export const userRouter = router({
         where: {
           NOT: [
             { id: ctx.session.user.id },
-            { email: "nancy.zegarski@gmail.com@gmail.com" },
+            { email: "nancy.zegarski@gmail.com" },
           ],
         },
         orderBy: { name: "asc" },
       });
-    } else if (
-      ctx.session.user.email === "nancy.zegarski@gmail.com@gmail.com"
-    ) {
+    } else if (ctx.session.user.email === "nancy.zegarski@gmail.com") {
       return ctx.prisma.user.findMany({
         where: {
           NOT: [{ id: ctx.session.user.id }, { email: "fettermann@gmail.com" }],
